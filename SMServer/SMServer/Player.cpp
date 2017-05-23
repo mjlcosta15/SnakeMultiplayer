ab7d8d0c047a3091ee17012f73b00997436ed6f0
Mário Costa <mariocosta@easyclick.pt>
2017-05-23 16:09:53 +0100

    added more logic to player and server

--- SMServer/SMServer/Player.cpp
+++ SMServer/SMServer/Player.cpp
@@ -1,12 +1,67 @@
 #include "Player.h"
 
 
-
-Player::Player()
+Player::Player(string name, int pid) : playerPID(pid), playerName(name), points(0)
 {
 }
-
 
 Player::~Player()
 {
 }
+
+int Player::getPoints() const
+{
+	return points;
+}
+
+int Player::getPosX() const
+{
+	return posX;
+}
+
+int Player::getPosY() const
+{
+	return posY;
+}
+
+int Player::getSnakeSize() const
+{
+	return snakeSize;
+}
+
+string Player::getName()
+{
+	return playerName;
+}
+
+void Player::addPoints(unsigned int points)
+{
+	this->points += points;
+}
+
+void Player::removePoints(unsigned int points)
+{
+	this->points -= points;
+}
+
+void Player::setPosition(int x, int y)
+{
+	posX = x;
+	posY = y;
+}
+
+void Player::decreaseSnakeSize(unsigned int num)
+{
+	snakeSize -= num;
+}
+
+void Player::increaseSnakeSize(unsigned int num)
+{
+	snakeSize += num;
+}
+
+void Player::setName(string name)
+{
+	playerName = name;
+}
+
--- SMServer/SMServer/Player.h
+++ SMServer/SMServer/Player.h
@@ -1,11 +1,30 @@
 #pragma once
+#include "MainHeader.h"
+
 class Player
 {
-	unsigned int playerID;
+	int playerPID;
+	string playerName;
 	int points;
-
+	int posX;
+	int posY;
+	int snakeSize;
 public:
-	Player();
+	Player(string name, int pid);
 	~Player();
+
+	int getPoints() const;
+	int getPosX() const;
+	int getPosY() const;
+	int getSnakeSize() const;
+	string getName();
+
+	void addPoints(unsigned int points);
+	void removePoints(unsigned int points);
+	void setPosition(int x, int y);
+	void decreaseSnakeSize(unsigned int num);
+	void increaseSnakeSize(unsigned int num);
+	void setName(string name);
+
 };
 
